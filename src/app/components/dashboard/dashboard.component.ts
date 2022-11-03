b84fbb14eb7628a5458fe4a1d905ea9cbb31bfad
Khanh Le <khanhle@unomaha.edu>
2022-11-02 22:17:06 -0500

    Search by RoomNumber

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  searchForm = new FormGroup({
+    inputValue: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(private roomService: RoomService) {}
 
   onSubmit() {
-    console.log("Onsubmit works!");
-  };
-
-  dashSearch() {
-    console.log("Searching...");
-  };
-
-  ngOnInit(): void {
+    console.log('Onsubmit works!');
+    console.log(this.searchForm.value.inputValue);
+    this.roomService
+      .getScheduleByRoomNumber(this.searchForm.value.inputValue)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 
+  dashSearch() {
+    console.log('Searching...');
+  }
+
+  ngOnInit(): void {}
 }
