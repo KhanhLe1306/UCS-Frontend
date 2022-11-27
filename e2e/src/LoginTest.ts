62b4e765ebba80d4bac3d36733dfd4580f9d9c51
Zach Randolph <zrandolph977@gmail.com>
2022-11-27 11:57:32 -0600

    Got testing to work! I decided to switch up what we used to test and decided to use Protractor with the Jasmine framework becaues that is a normal end 2 end testing framework used for a lot of angular projects. I have one test in there right now that was just used to test things out but will eventually need to be changed once we get those functions to work.

---
+++ e2e/src/LoginTest.ts
@@ -0,0 +1,27 @@
+import { browser, by, element, Key, logging, ExpectedConditions as EC } from 'protractor';
+
+describe('katalon', () => {
+
+	beforeAll(async () => { });
+	beforeEach(async () => { });
+
+	it('should do something', async () => {
+		await browser.get('http://localhost:4200/login');
+		await element(by.id("userName")).click();
+		await element(by.id("userName")).sendKeys('a');
+		await element(by.id("password")).click();
+		await element(by.id("password")).sendKeys('a');
+		await element(by.id("submitButton")).click();
+		await browser.get('http://localhost:4200/dashboard');
+		await element(by.id("logOut")).click();
+	});
+
+	afterEach(async () => {
+		// Assert that there are no errors emitted from the browser
+		const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+		expect(logs).not.toContain(jasmine.objectContaining({
+			level: logging.Level.SEVERE,
+		} as logging.Entry));
+	});
+
+});
