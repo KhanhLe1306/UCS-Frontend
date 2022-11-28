a5b132b29fbb27d5ad98e1987949231224297f4e
Khanh Le <khanhle@unomaha.edu>
2022-11-28 14:08:39 -0600

    Accept changes through modal popup

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  scheduleClass: any;
 
-  constructor(public modalService: ModalService) { }
+  editForm = new FormGroup({
+    startTime: new FormControl(),
+    endTime: new FormControl(),
+    meetingDays: new FormControl(),
+  });
+
+  constructor(public modalService: ModalService) {}
 
   ngOnInit(): void {
+    this.scheduleClass = this.modalService.scheduleClass;
+    console.log(this.scheduleClass);
+
+    this.editForm.setValue({
+      startTime: this.scheduleClass.startTime,
+      endTime: this.scheduleClass.endTime,
+      meetingDays: this.scheduleClass.meetingDays,
+    });
+    //console.log(this.editForm);
   }
 
+  onSave() {
+    this.modalService.showModal = false;
+    console.log(this.editForm.value);
+  }
 }
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -67,4 +67,9 @@ export class SearchResultsComponent implements OnInit {
     ];
   }
 
+  showModal(scheduleClass: any) {
+    this.modalService.showModal = true;
+    this.modalService.scheduleClass = scheduleClass;
+  }
+
 }
