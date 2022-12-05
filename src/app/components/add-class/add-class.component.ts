d05a6fac8ec30077279c235fc65d3d0ddb53ac3b
Khanh Le <khanhle@unomaha.edu>
2022-12-04 23:27:30 -0600

    UCS-83: AddClass change parameter names

--- src/app/components/add-class/add-class.component.ts
+++ src/app/components/add-class/add-class.component.ts
@@ -12,11 +12,11 @@ import { SharedDataService } from 'src/app/services/shared.service';
 @Component({
   selector: 'app-add-class',
   templateUrl: './add-class.component.html',
-  styleUrls: ['./add-class.component.css']
+  styleUrls: ['./add-class.component.css'],
 })
 export class AddClassComponent implements OnInit {
   results = {};
-  selectedMessage:any;
+  selectedMessage: any;
   addClassForm = new FormGroup({
     courseNum: new FormControl(''),
     section: new FormControl(''),
@@ -34,7 +34,11 @@ export class AddClassComponent implements OnInit {
     sat: new FormControl(''),
   });
 
-  constructor(private addService: AddService, private sharedDataService: SharedDataService, private router: Router) {
+  constructor(
+    private addService: AddService,
+    private sharedDataService: SharedDataService,
+    private router: Router
+  ) {
     /**
      * @param  {} 'home'
      * @param  {} ;addButton?.addEventListener('click'
@@ -44,9 +48,7 @@ export class AddClassComponent implements OnInit {
     addButton?.addEventListener('click', this.onSubmit);
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   onSubmit() {
     let days = [];
@@ -68,20 +70,22 @@ export class AddClassComponent implements OnInit {
 
     let daysjoin = days.join(',');
     console.log(daysjoin);
-    this.addService.addClass({
-      'cls': this.addClassForm.value.courseNum,
-      'section': this.addClassForm.value.section,
-      'instructor': this.addClassForm.value.instructor,
-      'classSize': this.addClassForm.value.classSize,
-      'classStart': this.addClassForm.value.classStart,
-      'classEnd': this.addClassForm.value.classEnd,
-      'roomCode': this.addClassForm.value.roomCode,
-      'room': this.addClassForm.value.room,
-      'days': daysjoin
-    }).subscribe((res) => {
-      this.sharedDataService.changeMessage(JSON.stringify(res));
-      this.router.navigate(['/success']);
-    });
-    console.log("OnSubmit Works! -> AddClass");
-  };
+    this.addService
+      .addClass({
+        courseNumber: this.addClassForm.value.courseNum,
+        sectionNumber: this.addClassForm.value.section,
+        instructorName: this.addClassForm.value.instructor,
+        enrollment: this.addClassForm.value.classSize,
+        classStart: this.addClassForm.value.classStart,
+        classEnd: this.addClassForm.value.classEnd,
+        roomCode: this.addClassForm.value.roomCode,
+        roomNumber: this.addClassForm.value.room,
+        days: daysjoin,
+      })
+      .subscribe((res) => {
+        this.sharedDataService.changeMessage(JSON.stringify(res));
+        this.router.navigate(['/success']);
+      });
+    console.log('OnSubmit Works! -> AddClass');
+  }
 }
